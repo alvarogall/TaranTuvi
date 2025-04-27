34c902179d7cceac80a621c15aa7f852acd26905
maxiprados <160849653+maxiprados@users.noreply.github.com>
2025-04-27 17:47:01 +0200

    UsuarioPremium / Usuario normal -> Novedades operativas

--- src/main/java/es/uma/taw/tarantuvi/controller/UsuarioPremium.java
+++ src/main/java/es/uma/taw/tarantuvi/controller/UsuarioPremium.java
@@ -3,6 +3,7 @@ package es.uma.taw.tarantuvi.controller;
 import es.uma.taw.tarantuvi.dao.PeliculaRepository;
 import es.uma.taw.tarantuvi.entity.PeliculaEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +20,11 @@ public class UsuarioPremium {
     @GetMapping("/usuarioPlus")
     public String usuarioPlus(Model model) {
         List<PeliculaEntity> peliculas = peliculaRepository.findAll();
+        List<PeliculaEntity> novedades = peliculaRepository.findPeliculasMasRecientes(PageRequest.of(0, 2));
         model.addAttribute("peliculas", peliculas);
+        model.addAttribute("novedades", novedades);
 
-        return "inicioUsuarioPremium";
+        return "UsuarioPremium/inicioUsuarioPremium";
     }
 
 
