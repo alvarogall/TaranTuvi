13d31a04d341c98994d307027d9ebbbf6fb218a9
Zemag_17 <tabletpablo26@gmail.com>
2025-05-04 13:13:39 +0200

    Vistas de inicio, géneros y productoras para administrador creadas

--- src/main/java/es/uma/taw/tarantuvi/controller/Administrador.java
+++ src/main/java/es/uma/taw/tarantuvi/controller/Administrador.java
@@ -1,7 +1,42 @@
 package es.uma.taw.tarantuvi.controller;
 
+import es.uma.taw.tarantuvi.dao.GeneroPeliculaRepository;
+import es.uma.taw.tarantuvi.dao.ProductoraRepository;
+import es.uma.taw.tarantuvi.entity.GeneroPeliculaEntity;
+import es.uma.taw.tarantuvi.entity.ProductoraEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class Administrador {
+    @Autowired
+    protected GeneroPeliculaRepository generoPeliculaRepository;
+
+    @Autowired
+    protected ProductoraRepository productoraRepository;
+
+    @GetMapping("/administrador")
+    public String vistaAdministrador() {
+        return "Administrador/inicioAdministrador";
+    }
+
+    @GetMapping("/administrador/generos")
+    public String vistaGeneros(Model model) {
+        List<GeneroPeliculaEntity> generos = generoPeliculaRepository.findAll();
+        model.addAttribute("generos", generos);
+        return "Administrador/generos";
+    }
+
+    @GetMapping("/administrador/productoras")
+    public String vistaProductoras(Model model) {
+        List<ProductoraEntity> productoras = productoraRepository.findAll();
+        model.addAttribute("productoras", productoras);
+        return "Administrador/productoras";
+    }
+
+
 }
---
+++ src/main/webapp/WEB-INF/view/Administrador/inicioAdministrador.jsp
@@ -0,0 +1,32 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: table
+  Date: 03/05/2025
+  Time: 21:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Inicio Administrador</title>
+    <link rel="stylesheet" type="text/css" href="/css/Editor/inicioEditor.css">
+</head>
+<body>
+  <jsp:include page="navBarAdministrador.jsp">
+    <jsp:param name="activePage" value="inicio"/>
+  </jsp:include>
+
+  <div class="welcome-container">
+    <h1>Bienvenido, Administrador 👋</h1>
+    <p>Como admistrador, tienes acceso a las siguientes funciones:</p>
+
+    <ul class="features-list">
+      <li>➕ Añadir nuevos géneros y productoras.</li>
+      <li>✏️ Editar información existente.</li>
+      <li>🗑️ Eliminar registros obsoletos o incorrectos.</li>
+      <li>🔍 Buscar por nombre o filtrar alfabéticamente.</li>
+    </ul>
+  </div>
+
+</body>
+</html>
