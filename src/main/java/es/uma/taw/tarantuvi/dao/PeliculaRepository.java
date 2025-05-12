bc4b13936eac08d44d291e6b197edf295c1a1403
alvarogall <varosg@proton.me>
2025-05-12 12:23:33 +0200

    Recuperacion

--- src/main/java/es/uma/taw/tarantuvi/dao/PeliculaRepository.java
+++ src/main/java/es/uma/taw/tarantuvi/dao/PeliculaRepository.java
@@ -11,4 +11,10 @@ public interface PeliculaRepository extends JpaRepository<PeliculaEntity, Intege
     @Query("SELECT p FROM PeliculaEntity p ORDER BY p.fechaestreno DESC")
     List<PeliculaEntity> findPeliculasMasRecientes(Pageable pageable);
 
+    @Query("SELECT p FROM PeliculaEntity p " +
+            "JOIN p.generoPeliculaList g " +
+            "WHERE (:generoId IS NULL OR g.id = :generoId) " +
+            "AND (:idiomaId IS NULL OR p.idiomaoriginalhabladoid.id = :idiomaId)")
+    List<PeliculaEntity> findPeliculasByFiltros(Integer generoId, Integer idiomaId);
+
 }
